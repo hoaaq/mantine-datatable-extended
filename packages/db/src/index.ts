371662a865db378a883f1c735fdcfda8472fcb6b
hoaaq <auhoa1997@gmail.com>
2025-12-25 09:39:40 +0700

    Add pnpm lockfile and update TypeScript configuration

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 
+// biome-ignore lint/performance/noNamespaceImport: no reason
 import * as schema from "./schema";
 
 export const db = drizzle(process.env.DATABASE_URL || "", { schema });
